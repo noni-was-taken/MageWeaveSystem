ee6a937873971a4f883e1c068b24474bb4a7e0c4
noni-was-taken <ninocalunod.info@gmail.com>
2025-07-10 00:00:42 +0800

    full reset, everything works now

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MageWeave</title>
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+</head>
+<body class="bg-blue text-gray-900">
+
+</body>
